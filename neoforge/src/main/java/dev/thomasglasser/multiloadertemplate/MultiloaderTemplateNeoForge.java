f2b75bb70239dac26d68a16f3821ace7ad66b439
Thomas Glasser <tjglass06@gmail.com>
2024-07-03 00:44:14 -0400

    Added spotless with NeoForge standard Updated deps

--- neoforge/src/main/java/dev/thomasglasser/multiloadertemplate/MultiloaderTemplateNeoForge.java
+++ neoforge/src/main/java/dev/thomasglasser/multiloadertemplate/MultiloaderTemplateNeoForge.java
@@ -4,8 +4,7 @@ import net.neoforged.bus.api.IEventBus;
 import net.neoforged.fml.common.Mod;
 
 @Mod(MultiloaderTemplate.MOD_ID)
-public class MultiloaderTemplateNeoForge
-{
+public class MultiloaderTemplateNeoForge {
     public MultiloaderTemplateNeoForge(IEventBus modBus) {
         MultiloaderTemplate.init();
     }
